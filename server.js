ba0676d41c6bfae21d42e55d04d98ea66a804579
ADanilo7 <daniloreriutaba@gmail.com>
2020-06-01 10:33:56 -0300

    teste para exibir valor4

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+
 const app = express();
 console.log("REACT_API = "+process.env.API_URL);
+const port = process.env.API_URL || 8080;
 
 
 // the __dirname is the current directory from where the script is running
